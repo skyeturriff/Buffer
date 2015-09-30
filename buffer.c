794b5d521eb390386c993568a58ba4b3cabecf56
Skye Turriff <turriff.skye@gmail.com>
2015-09-30 16:45:59 -0400

    initial commit

---
+++ buffer.c
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <limits.h>
+#include "buffer.h"
+
+
+Buffer* b_create(short init_capacity, char inc_factor, char o_mode) {
+
+	/* In order to ensure the value of inc_factor is within 0-255, cast 
+	to an unsigned char. If function is called with values outside of this 
+	range (ie -1, 256), internally the values will be converted to be within 
+	UCHAR_MIN and UCHAR_MAX. For example, if 256 is passed as inc_factor, it 
+	will be truncated and interpreted by the function as 0. 
+	See C99 Standard 6.3.1.3 */
+	unsigned char uc_inc_factor = inc_factor;
+
+	Buffer* pBD = NULL;	/* Declare pointer to BufferDescriptor struct type */
+
+	switch (o_mode) {	/* Using Svillen's test for valid o_mode */
+	case 'a': 
+	case 'f': 
+	case 'm':
+		break;
+	default:
+		return NULL;
+	}
+
+	/* Test for valid init_capacity and inc_factor arguments.
+	If a number greater than SHRT_MAX is passed for init_capacity, overflow 
+	will cause variable to become negative. */
+	if (init_capacity <= 0 ||  ((o_mode == 'm') && (uc_inc_factor > 100)))	
+		return NULL;
+
+	/* All arguments valid. Try to allocate memory for one Buffer structure */
+	pBD = (Buffer *)calloc(1, sizeof(Buffer));
+
+	/* Check for successful allocation */
+	if (pBD == NULL)	
+		return NULL;
+
+	/* Try to allocate memory for one dynamic character buffer */
+	pBD->cb_head = (char *)malloc(init_capacity);
+
+	/* Check for successful allocation */
+	if (pBD->cb_head == NULL)	
+		return NULL;
+	
+	/* Set buffer operational mode and increment factor */
+	if (uc_inc_factor == 0 || o_mode == 'f') {
+		pBD->mode = 0;
+		pBD->inc_factor = 0;
+	}
+	else if (o_mode == 'm') {
+		pBD->mode = -1;
+		pBD->inc_factor = inc_factor;
+	}
+	else if (o_mode == 'a') {
+		pBD->mode = 1;
+		pBD->inc_factor = inc_factor;
+	}
+
+	/* Set buffer starting capacity */
+	pBD->capacity = init_capacity;
+
+	return pBD;
+}
+
+pBuffer b_addc(pBuffer const pBD, char symbol) {
+
+	/* Check for operational buffer */
+	if (pBD == NULL || pBD->cb_head == NULL)
+		return NULL;
+
+	/* If there is room in the buffer, try to add the symbol */
+	if (pBD->addc_offset < pBD->capacity) {
+		pBD->r_flag = 0;
+		pBD->cb_head[pBD->addc_offset++] = symbol;
+		return pBD;
+	}
+
+	/* Buffer is full and cannot be resized when in fixed mode */
+	if (pBD->mode == 0)
+		return NULL;
+
+	/* Buffer is full and in additive mode. Attempt to resize capacity */
+	
+
+}
+
+int b_reset(Buffer* const pBD) {
+
+}
+
+void b_destroy(Buffer* const pBD) {
+	free(pBD->cb_head);
+	free(pBD);
+}
+
+int b_isFull(Buffer* const pBD) {
+	/*Check for run-time errors. If an error occurs, RETURN -1*/
+
+	return pBD->capacity == pBD->addc_offset ? 0 : 1;
+}
+
+short b_size(Buffer* const pBD) {
+	/*Check fro run-time errors. If an error occurs, RETURN -1*/
+
+	return pBD->addc_offset;
+}
+
+short b_capacity(Buffer* const pBD) {
+	/*Check for run-time errors. If an erro occurs, RETURN -1*/
+		
+	return pBD->capacity;
+}
+
+char* b_setmark(Buffer* const pBD, short mark) {
+	/*If funtion cannot complete task (run-time error), RETURN NULL*/
+
+	if (0 <= mark && mark <= pBD->addc_offset){
+		pBD->mark_offset = mark;
+		return &(pBD->cb_head[pBD->mark_offset]);
+	}
+	else
+		return NULL;
+}
+
+short b_mark(Buffer* const pBD) {
+	/*Check for run-time errors. If an error occurs, RETURN -1*/
+
+	return pBD->mark_offset;
+}
+
+int b_mode(Buffer* const pBD) {
+	/*Check for run-time errors. If an error occurs, notify the calling function of the error*/
+
+	return pBD->mode;
+}
+
+size_t b_inc_factor(Buffer* const pBD) {
+	/*Check for run-time errors. If an error occurs, RETURN 256*/
+
+	return (unsigned char)pBD->inc_factor; /*Return NON_NEGATIVE VALUE of inc_factor*/
+}
+
+int b_load(FILE* const fi, Buffer* const pBD) {
+	return R_FAIL_1;
+}
+
+int b_isempty(Buffer* const pBD) {
+	/*Check for run-time errors. If an error occurs, RETURN -1*/
+
+	return pBD->addc_offset == 0 ? 1 : 0;
+}
+
+int b_eob(Buffer* const pBD) {
+	/*Check for run-time errors. If an error occurs, RETURN -1*/
+
+	return pBD->eob;
+}
+
+char b_getc(Buffer* const pBD) {
+	/*Check argument for validity (possible run-time error). 
+	If invalid, RETURN -2*/
+
+	if (pBD->getc_offset == pBD->addc_offset) {
+		pBD->eob = 1;
+		return -1;
+	}
+	else {
+		pBD->eob = 0;
+		char ch = pBD->cb_head[pBD->getc_offset];
+		pBD->getc_offset++;
+		return ch;
+	}
+}
+
+int b_print(Buffer* const pBD) {
+
+}
+
+Buffer *b_pack(Buffer* const pBD) {
+	
+}
+
+char b_rflag(Buffer* const pBD) {
+	/*Check for run-time errors. If an error occurs, RETURN -1*/
+
+	return pBD->r_flag;
+}
+
+short b_retract(Buffer* const pBD) {
+	/*Check for run-time errors. If an error occurs, RETURN -1*/
+
+	return --(pBD->getc_offset);
+}
+
+short b_retract_to_mark(Buffer* const pBD) {
+	/*Check for run-time errors. If an error occurs, RETURN -1*/
+
+	return pBD->getc_offset = pBD->mark_offset;
+}
+
+short b_getc_offset(Buffer* const pBD) {
+	/*Check for run-time errors. If an error occurs, RETURN -1*/
+
+	return pBD->getc_offset;
+}
---
+++ buffer_test.c
@@ -0,0 +1,34 @@
+#include "buffer.h"
+
+int main(int argc, char **argv) {
+	Buffer *ptr_Buffer;   /* pointer to Buffer structure */
+	/*ptr_Buffer = b_create(*argv[1], *argv[2], *argv[3]);*/
+	ptr_Buffer = b_create(SHRT_MAX, 15, 'a');
+
+	b_addc(ptr_Buffer, 'a');
+	b_addc(ptr_Buffer, 'a');
+	b_addc(ptr_Buffer, 'a');
+
+	/*
+	printf("capacity: %d\n", *argv[1]);
+	printf("inc_factor char: %c\tinc_factor int: %d\n", *argv[2], *argv[2]);
+	printf("mode car: %c\n", argv[3]);
+	*/
+	if (ptr_Buffer == NULL)
+		printf("Could not create buffer\n");
+	else {
+		printf("capacity + inc_factor: %d\n", ptr_Buffer->capacity + ptr_Buffer->inc_factor);
+		printf("capacity + us_inc_factor: %d\n", ptr_Buffer->capacity + (int)ptr_Buffer->inc_factor);
+		printf("\nPrinting buffer parameters:\n\n");
+		printf("The capacity of the buffer is:  %d\n", /*ptr_Buffer->capacity*/ b_capacity(ptr_Buffer));
+		printf("The current size of the buffer is:  %d\n", /*ptr_Buffer->addc_offset*/ b_size(ptr_Buffer));
+		printf("The operational mode of the buffer is:   %d\n", /*ptr_Buffer->mode*/ b_mode(ptr_Buffer));
+		printf("The increment factor of the buffer is:  %u\n", /*ptr_Buffer->inc_factor*/ b_inc_factor(ptr_Buffer));
+		printf("The current mark of the buffer is:  %d\n", /*ptr_Buffer->mark_offset*/ b_mark(ptr_Buffer));
+		/*printf("The reallocation flag is:   %d\n",b_rflag(ptr_Buffer));*/
+		printf("\nPrinting buffer contents:\n\n");
+		b_print(ptr_Buffer);
+	}
+
+	return 0;
+}
